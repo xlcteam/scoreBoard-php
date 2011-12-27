0a93001149e58aa52c6a82fc870033d4c68bf0e1
Ado Matejov <a.matejov@centrum.sk>
2011-12-27 13:01:06 +0100

    pause timer on goal

--- js/goals.js
+++ js/goals.js
@@ -4,11 +4,17 @@ var goal2 = 0;
 function team1Goal()
 {
 	goal1++;
+	if (document.getElementById("btnStart").innerHTML == "Stop"){
+    		document.getElementById("btnStart").innerHTML = "Start";
+	}
 	$("#team1").html(goal1);
 }
 
 function team2Goal()
 {
 	goal2++;
+	if (document.getElementById("btnStart").innerHTML == "Stop"){
+    		document.getElementById("btnStart").innerHTML = "Start";
+	}
 	$("#team2").html(goal2);
 }
