5138da649d396f834d2760436aeabaecae2c9d0f
Ado Matejov <a.matejov@centrum.sk>
2012-01-03 14:20:28 +0100

    toggle function (called after goal)

--- js/goals.js
+++ js/goals.js
@@ -1,20 +1,26 @@
 var goal1 = 0;
 var goal2 = 0;
 
+function toggleOnGoal()
+{
+	btnStart = document.getElementById("btnStart");
+	$("#time").stopwatch().stopwatch('stop');
+	
+	if (btnStart.innerHTML == "Pause"){
+		btnStart.innerHTML = "Resume";
+	}
+}
+
 function team1Goal()
 {
 	goal1++;
-	if (document.getElementById("btnStart").innerHTML == "Stop"){
-    		document.getElementById("btnStart").innerHTML = "Start";
-	}
+	toggleOnGoal();
 	$("#team1").html(goal1);
 }
 
 function team2Goal()
 {
 	goal2++;
-	if (document.getElementById("btnStart").innerHTML == "Stop"){
-    		document.getElementById("btnStart").innerHTML = "Start";
-	}
+	toggleOnGoal();
 	$("#team2").html(goal2);
 }
