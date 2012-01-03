1d44f49bc54fd59e2a0be4f136bf05f847fcc779
mr.Shu <mrshux@gmail.com>
2012-01-03 15:52:51 +0000

    fixed time

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -3,7 +3,7 @@ function toggle ()
   btnStart = document.getElementById("btnStart");  
 	if (btnStart.innerHTML == "Start" || btnStart.innerHTML == "Resume"){
     btnStart.innerHTML = "Pause";
-		$("#time").stopwatch().stopwatch('start');
+		$("#time").stopwatch({formatter: format, updateInterval: 50}).stopwatch('start');
     return;
   }
   if (btnStart.innerHTML == "Pause" || btnStart.innerHTML == "Resume"){
@@ -13,6 +13,20 @@ function toggle ()
   }
 }
 
+function pad2(number) {                                                
+     return (number < 10 ? '0' : '') + number;                         
+}                                                                      
+
+function format(millis) {                                              
+    var seconds, minutes;                                              
+    minutes = Math.floor(millis / 60000);                              
+    millis %= 60000;                                                   
+    seconds = Math.floor(millis / 1000);                               
+    millis = Math.floor(millis % 1000);                                
+    millis = Math.floor(millis / 10);                                  
+    return [pad2(minutes), pad2(seconds)].join(':') + ',' + pad2(millis);
+}   
+
 function resetTime()
 {
   btnStart = document.getElementById("btnStart");	
@@ -20,4 +34,5 @@ function resetTime()
 	$("#time").stopwatch().stopwatch('reset');
 	$("#time").html("00:00:00");
 	btnStart.innerHTML = "Start";
+  return;
 }
