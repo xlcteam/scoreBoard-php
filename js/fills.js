885f9ac2c7b4ebd348c85ac834690568179c0cdc
Ado Matejov <a.matejov@centrum.sk>
2012-01-03 16:52:52 +0100

    fills right or left div with color

---
+++ js/fills.js
@@ -0,0 +1,23 @@
+function fill1()
+{
+	$(".leftBckg").css("opacity", "0.7");
+	$(".goalLTex").css("opacity", "1");
+}
+
+function fill2()
+{
+	$(".rightBckg").css("opacity", "0.7");
+	$(".goalRTex").css("opacity", "1");
+}
+
+function unfill1()
+{
+	$(".leftBckg").css("opacity", "0");
+	$(".goalLTex").css("opacity", "0");
+}
+
+function unfill2()
+{
+	$(".rightBckg").css("opacity", "0");
+	$(".goalRTex").css("opacity", "0");
+}
